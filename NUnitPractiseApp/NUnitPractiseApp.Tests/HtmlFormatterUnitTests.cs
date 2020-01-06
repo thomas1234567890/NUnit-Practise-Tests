2e5d058fb02c03df229a1f6da91c868863919638
Thomas <tompatcon@gmail.com>
2020-01-06 21:44:08 +0000

    Code for string test

--- NUnitPractiseApp/NUnitPractiseApp.Tests/HtmlFormatterUnitTests.cs
+++ NUnitPractiseApp/NUnitPractiseApp.Tests/HtmlFormatterUnitTests.cs
@@ -1,14 +1,31 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace NUnitPractiseApp.Tests
 {
-    [TestClass]
-    public class UnitTest1
+    [TestFixture]
+    public class HtmlFormatterUnitTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseStringWithStrongElement()
         {
+            ///arange
+            var formater = new HtmlFormatter();
+            ///act
+            var result = formater.FormatAsBold("A to Z");
+            ///assert
+
+            ///specific
+            //Assert.That(result, Is.EqualTo("<strong>A to Z</strong>"));
+            /// below ignores case
+            Assert.That(result, Is.EqualTo("<strong>A to Z</strong>").IgnoreCase);
+
+            ///general
+            //Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("A to Z"));
         }
     }
 }
