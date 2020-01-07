4bd1d9872e1e90e714091e67b7934e8796c3e898
Thomas <tompatcon@gmail.com>
2020-01-07 23:06:31 +0000

    Testing Void Methods
    Unit Tests for ErrorLogger class with a focus on how to handle void methods and exception handling.
    Tests available in both All and ErrorLoggerUnitTests playlist.

---
+++ NUnitPractiseApp/NUnitPractiseApp.Tests/ErrorLoggerUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace NUnitPractiseApp.Tests
+{
+    [TestFixture]
+    public class ErrorLoggerUnitTests
+    {
+
+        ///setup
+        private ErrorLogger _errorLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _errorLogger = new ErrorLogger();
+        }
+        [Test]
+        [TestCase("This is an errrrorrrr.")]
+        public void Log_WhenCalled_SetTheLastErrorProperty(string LoggedError)
+        {
+            ///act
+            _errorLogger.Log(LoggedError);
+            ///assert
+            ///method is void so check the object it changes, the property LastError.
+            Assert.That(_errorLogger.LastError, Is.EqualTo("xxx"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log__InvalidError__ThrowNewArgumentNullException(string error)
+        {
+            ///act
+            /// We use delegates/lamda expressions to test for exceptions
+            ///assert
+            Assert.That(()=> _errorLogger.Log(error), Throws.ArgumentNullException);
+            //Assert.That(()=> _errorLogger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+    }
+}
