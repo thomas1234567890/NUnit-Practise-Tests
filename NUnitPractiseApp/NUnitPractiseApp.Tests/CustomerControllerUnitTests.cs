ef2c712108790abbb022b8cd25380c07357e22f9
Thomas <tompatcon@gmail.com>
2020-01-07 12:40:52 +0000

    Customer Controller Unit Test
    Testing the return type of the Customer Controller methods.

---
+++ NUnitPractiseApp/NUnitPractiseApp.Tests/CustomerControllerUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace NUnitPractiseApp.Tests
+{
+    [TestFixture]
+    public class CustomerControllerUnitTests
+    {
+        [Test]
+        public void GetCustomer__When_Id_Is_Zero__ReturnNotFound()
+        {
+            ///arange
+            var customerController = new CustomerController();
+            ///act
+            var result = customerController.GetCustomer(0);
+            ///assert
+            ///Type Of means it can be a NotFound object only.
+            Assert.That(result, Is.TypeOf<NotFound>());
+            ///Instance Of means it can be a NotFound object OR one of its derivitives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer__When_Id_Is_Not_Zero__ReturnOK()
+        {
+            ///arange
+            var customerController = new CustomerController();
+            ///act
+            var result = customerController.GetCustomer(1);
+            ///assert
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
