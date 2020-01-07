0c9fac910328af4e5da34033bad4fdf9dd3266e1
Thomas <tompatcon@gmail.com>
2020-01-07 13:03:20 +0000

    Refactored Customer Controler Unit Test
    Added an NUnit setup.
    Paramemetered Status OK Test with TestCases.

--- NUnitPractiseApp/NUnitPractiseApp.Tests/CustomerControllerUnitTests.cs
+++ NUnitPractiseApp/NUnitPractiseApp.Tests/CustomerControllerUnitTests.cs
@@ -7,27 +7,35 @@ namespace NUnitPractiseApp.Tests
     [TestFixture]
     public class CustomerControllerUnitTests
     {
+        /// Setup to reduce reused code
+        private TestNinja.Fundamentals.CustomerController _customerController;
+
+        [SetUp]
+        ///make sure signiture is public
+        public void Setup()
+        {
+            _customerController = new CustomerController();
+        }
+
         [Test]
         public void GetCustomer__When_Id_Is_Zero__ReturnNotFound()
         {
-            ///arange
-            var customerController = new CustomerController();
             ///act
-            var result = customerController.GetCustomer(0);
+            var result = _customerController.GetCustomer(0);
             ///assert
             ///Type Of means it can be a NotFound object only.
             Assert.That(result, Is.TypeOf<NotFound>());
             ///Instance Of means it can be a NotFound object OR one of its derivitives
-            //Assert.That(result, Is.InstanceOf<NotFound>());
+            ///Assert.That(result, Is.InstanceOf<NotFound>());
         }
 
         [Test]
-        public void GetCustomer__When_Id_Is_Not_Zero__ReturnOK()
+        [TestCase(1)]
+        [TestCase(2)]
+        public void GetCustomer__When_Id_Is_Not_Zero__ReturnOK(int a)
         {
-            ///arange
-            var customerController = new CustomerController();
             ///act
-            var result = customerController.GetCustomer(1);
+            var result = _customerController.GetCustomer(a);
             ///assert
             Assert.That(result, Is.TypeOf<Ok>());
         }
